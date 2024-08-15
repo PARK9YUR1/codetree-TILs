7e92905044e28adda30a2ba65b60603a8c4feea6
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-08-15 22:34:34 +0900

    Title: 길이 단위 변환하기, Time: 162ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240815/길이 단위 변환하기/change-length-unit.js
@@ -0,0 +1,11 @@
+const ft = 30.48;
+const mi = 160934;
+
+const x = 9.2;
+const y = 1.3;
+
+const a = (x * ft).toFixed(1);
+const b = (y * mi).toFixed(1);
+
+console.log(`${x}ft = ${a}cm`);
+console.log(`${y}mi = ${b}cm`);
