e9a54bf9d889b43a898b63e28c2d7c8fd598d758
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-09-01 21:24:48 +0900

    Title: 합과 평균, Time: 177ms, Memory: 14MB, Status: Passed - CodeTree

--- 240901/합과 평균/sum-and-avg.js
+++ 240901/합과 평균/sum-and-avg.js
@@ -2,6 +2,6 @@ const fs = require("fs");
 
 let [a, b] = fs.readFileSync(0).toString().split(" ").map(num => parseInt(num));
 let sum = a+b;
-let avg = sum/2.0;
+let avg = sum/2;
 
-console.log(sum, avg);
+console.log(sum, avg.toFixed(1));
