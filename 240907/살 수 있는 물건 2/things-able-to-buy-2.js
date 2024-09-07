0f3fe49c9abae06b475211c38ccc4276f3a7e864
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-09-07 23:34:00 +0900

    Title: 살 수 있는 물건 2, Time: 256ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240907/살 수 있는 물건 2/things-able-to-buy-2.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+let n = fs.readFileSync(0).toString().trim();
+n = Number(n);
+
+let ans;
+
+if (n >= 3000) {
+    ans = "book";
+} else if (n >= 1000) {
+    ans = "mask";
+} else if (n >= 500) {
+    ans = "pen";
+} else {
+    ans = "no";
+}
+
+console.log(ans)
