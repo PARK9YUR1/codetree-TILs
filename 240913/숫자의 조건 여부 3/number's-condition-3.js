63f8b9d5dcaaaacdb6d7c59a7ec8b054887d3867
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-09-13 23:48:32 +0900

    Title: 숫자의 조건 여부 3, Time: 188ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240913/숫자의 조건 여부 3/number's-condition-3.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+let a = fs.readFileSync(0).toString().trim();
+a = parseInt(a);
+
+if (a % 13 === 0 || a % 19 === 0) {
+    console.log("True");
+} else {
+    console.log("False");
+}
