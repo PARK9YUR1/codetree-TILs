bfb02b7e58c77679de5526861041300087315497
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-09-13 23:49:35 +0900

    Title: 둘 중 하나의 배수, Time: 192ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240913/둘 중 하나의 배수/multiple-of-either.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+let a = fs.readFileSync(0).toString().trim();
+a = parseInt(a);
+
+if (a % 3 === 0 || a % 5 === 0) {
+    console.log(1);
+} else {
+    console.log(0);
+}
