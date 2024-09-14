23380c72e9c24ad5e5f94f5abcb171df677b1b58
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-09-14 20:57:16 +0900

    Title: 장학금, Time: 197ms, Memory: 47MB, Status: Passed - CodeTree

--- 240914/장학금/scholarship.js
+++ 240914/장학금/scholarship.js
@@ -5,9 +5,9 @@ let [x, y] = fs.readFileSync(0).toString().split(" ").map(num => Number(num));
 if (x < 90) {
     console.log(0);
 } else if (y >= 95) {
-    console.log(10);
+    console.log(100000);
 } else if (y >= 90) {
-    console.log(5);
+    console.log(50000);
 } else  {
     console.log(0);
 }
