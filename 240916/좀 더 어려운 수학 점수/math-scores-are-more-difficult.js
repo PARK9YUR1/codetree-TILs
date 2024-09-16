18cc590385af3c8a4eb414143ee3d2baea98d3a8
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-09-16 23:35:17 +0900

    Title: 좀 더 어려운 수학 점수, Time: 183ms, Memory: 47MB, Status: Passed - CodeTree

--- 240916/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
+++ 240916/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
@@ -2,14 +2,8 @@ const fs = require("fs");
 
 let [a, b] = fs.readFileSync(0).toString().split("\n").map(score => score.split(" ").map(s => Number(s)));
 
-if (a[0] > b[0]) {
+if (a[0] > b[0] || (a[0] === b[0] && a[1] > b[1])) {
     console.log("A");
-} else if (a[0] === b[0]) {
-    if (a[1] > b[1]) {
-        console.log("A");
-    } else {
-        console.log("B");
-    }
 } else  {
     console.log("B");
 }
