7afd34bfc1ab71304174cf07dd5581e4de0f8f33
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-10-08 00:00:18 +0900

    Title: 369 게임, Time: 195ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241008/369 게임/369-game.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+let n = fs.readFileSync(0).toString().trim();
+n = parseInt(n);
+let cur = 1;
+let res = "";
+
+while (cur <= n) {
+    if (cur % 3 === 0) {
+        res += "0 ";
+    } else if (cur.toString().indexOf("3") > 0 || cur.toString().indexOf("6") > 0 || cur.toString().indexOf("9") > 0) {
+        res += "0 ";
+    } else {
+        res += cur + " ";
+    }
+    cur++;
+}
+
+console.log(res);
